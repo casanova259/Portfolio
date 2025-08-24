6946dcffa38177b9bfec9f200d43cb23b96373b5
Manik Sharma <casanova270407@gmail.com>
2025-08-24 22:09:55 +0530

    changed the background and added a small heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-// import Navbar from "./Components/Navbar/Navbar.jsx";
-import LightRays from "./blocks/Backgrounds/LightRays/LightRays.jsx";
+import Navbar from "./Components/Navbar/Navbar.jsx";
 import Aurora from "./blocks/Backgrounds/Aurora/Aurora.jsx";
 import BlurText from "./blocks/TextAnimations/BlurText/BlurText.jsx";
+import SplitText from "./blocks/TextAnimations/SplitText/SplitText.jsx";
+import Galaxy from "./blocks/Backgrounds/Galaxy/Galaxy.jsx";
+
 function App() {
 
   const handleAnimationComplete = () => {
@@ -13,31 +15,43 @@ function App() {
 
     <div className="relative h-screen w-full overflow-hidden bg-black">
 
-
+      <Navbar />
       {/* Background */}
-      <div style={{ width: '100%', height: '600px', position: 'relative' }}>
-        <Aurora
-          colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
-          blend={0.5}
-          amplitude={1.0}
-          speed={0.5}
-        />
-
-      </div>
-
+      <Aurora
+        colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+        blend={0.5}
+        amplitude={1.0}
+        speed={0.5}
+      />
       {/* Foreground Content */}
       <div className="absolute inset-0 flex flex-col items-center justify-center z-10 text-center">
         <BlurText
-          text="Crafting Code, Creating Magic"
+          text="Crafting Code,Creating Magic"
           delay={200}
           animateBy="words"
           direction="top"
           onAnimationComplete={handleAnimationComplete}
-          className="text-7xl font-bold text-amber-300 text-center"
+          className="text-7xl font-bold text-white text-center"
         />
         {/* <p className="mt-4 text-lg md:text-xl text-gray-300">
           Exploring React, Animations & Creativity ✨
         </p> */}
+        <div className="pt-8 pb-8">
+          <SplitText
+            text="Hi, I’m Manik Sharma — a passionate learner and aspiring developer."
+            className="text-3xl  text-amber-300 "
+            delay={50}
+            duration={0.6}
+            ease="power3.out"
+            splitType="chars"
+            from={{ opacity: 0, y: 40 }}
+            to={{ opacity: 1, y: 0 }}
+            threshold={0.1}
+            rootMargin="-100px"
+            textAlign="center"
+            onLetterAnimationComplete={handleAnimationComplete}
+          />
+        </div>
       </div>
     </div>
   );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
 
     return (
-        <div className='fixed bg-transparent flex justify-between items-center gap-16 py-3 px-10 left-1/2 translate-x-[-50%]  top-[20px] rounded-full backdrop-blur-md bg-opacity-30 text-white shadow-lg z-10'>
+        <div className='fixed bg-transparent flex justify-between items-center gap-16 py-3 px-10 left-1/2 translate-x-[-50%]  top-[20px] rounded-full backdrop-blur-md bg-opacity-50 text-white shadow-lg z-10'>
             <ul className='flex gap-16 text-xl'>
                 <a>Home</a>
                 <a>About</a>
